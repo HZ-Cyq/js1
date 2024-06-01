72a1e6f6bf3bf07be8ea277c1c621a63a5b9d4e5
HZ-Cyq <1369509439@qq.com>
2024-06-01 10:05:25 +0800

    暂提

--- 1.js
+++ 1.js
@@ -1,6 +1,24 @@
-// let array = [1, 2, 3, 4, 5];
-// for(let a in array) {
-//     console.log(a);
+// // 定义数组
+// const array = ["a", "b"];
+
+// // 定义一个空对象
+// const object = {};
+
+// // 通过循环逐级创建嵌套对象
+// let temp = object;
+// for (const key of array) {
+//   temp[key] = {};
+//   temp = temp[key];
 // }
-let hello = "hello";
-console.log(`hello ${hello}`);
+
+// // 设置值
+// temp.value = "Hello World";
+
+// // 获取值
+// console.log(object.a.b.value); // 输出: Hello World
+
+let map = new Map();
+map.set("1", "111");
+map.set("2", "222");
+let array = Array.from(map.values());
+console.log(array);
---
+++ 恒歌/1.js
@@ -0,0 +1,9 @@
+// let now = new Date();
+// const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+// console.log(now.toLocaleString());
+// const formattedDate = now.toLocaleString('en-US', options).replace(',', '');
+// console.log(formattedDate);
+console.log("hello world1");
+setTimeout(() => {
+    console.log("hello world2");
+}, 3000);
